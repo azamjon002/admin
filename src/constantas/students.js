f68e2306a2c6699ddfe6d1f795dfc14f9d3ed9bf
azamjon002 <azamjon_oo2@inbox.ru>
2025-01-30 17:25:22 +0500

    students table

---
+++ src/constantas/students.js
@@ -0,0 +1,26 @@
+const Students = [
+    {
+        id: 1,
+        firstname:'Ahmadillo',
+        lastname: 'Olimov',
+        phoneNumber:'9989374737883',
+        status : true,
+        deleted_at: null
+    },
+    {
+        id: 2,
+        firstname:'Alisher',
+        lastname: 'Abdurahmonov',
+        phoneNumber:'9989374734783',
+        status : true,
+        deleted_at: null
+    },
+    {
+        id: 3,
+        firstname:'Muhammadsodiq',
+        lastname: 'Holmirzayev',
+        phoneNumber:'998937324783',
+        status : false,
+        deleted_at: null
+    }
+]
